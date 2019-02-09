21f80812e40fac51adec57ac819e639fc0f4b0e9
adam <adam6626@gmail.com>
2019-02-09 16:22:31 +0100

    add create player model in Game class

--- Game.cs
+++ Game.cs
@@ -3,9 +3,16 @@ using System;
 namespace Hangman
 {
     class Game{
+
+        private string nick;
         
         public void start(){
-            Console.WriteLine("Hello World!");
+            Console.Write("\nEnter your name: ");
+            nick = Console.ReadLine();
+            Player player = new Player(nick);
+            while(player.getLife() > 0){
+                 Console.WriteLine("\nHello world");
+            }
         }
     }
 }
