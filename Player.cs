dac057af19f3d5da573aa4fdb1fb3bbae822cd4d
adam <adam6626@gmail.com>
2019-02-09 16:05:52 +0100

    add constructor, getters and setter to Player class

--- Player.cs
+++ Player.cs
@@ -1,6 +1,31 @@
+using System;
+
 namespace Hangman
 {
     class Player{
-        
+
+        private string nick;
+        private int score;
+
+        public Player(string playerName){
+            nick = playerName;
+            score = 0;
+        }
+
+        public string getNick(){
+            return nick;
+        }
+
+        public void setNick(string nick){
+            this.nick = nick;
+        }
+
+        public int getScore(){
+            return score;
+        }
+
+        public void setScore(int score){
+            this.score = score;
+        }
     }
 }
