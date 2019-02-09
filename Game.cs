66fea0d8c07579dd96fcdc3156432319ec882294
adam <adam6626@gmail.com>
2019-02-09 16:46:26 +0100

    add a random password selection method in Game class

--- Game.cs
+++ Game.cs
@@ -1,18 +1,52 @@
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace Hangman
 {
     class Game{
 
         private string nick;
+        private string capital;
+        private string countryHint;
         
         public void start(){
             Console.Write("\nEnter your name: ");
             nick = Console.ReadLine();
             Player player = new Player(nick);
-            while(player.getLife() > 0){
-                 Console.WriteLine("\nHello world");
+            randomCapitalSelection();
+
+            if(player.getLife() > 0){
+                 Console.WriteLine("\nHello world " + capital + " " + countryHint);
             }
+        }
+
+        public void randomCapitalSelection(){
+            int countryIdx = 0;
+            int capitalIdx = 1;
+            string randomCapital = "";
+            string countryHint = "";
+
+            Random rnd = new Random();
+            List<string> capitals = new List<string>();
+            List<string> countries = new List<string>();
+
+            using (StreamReader sr = new StreamReader("countries_and_capitals.txt")){
+                string line;
+                while((line = sr.ReadLine()) != null){
+                    string[] dataSplit = line.Split('|');
+                    countries.Add(dataSplit[countryIdx]);
+                    capitals.Add(dataSplit[capitalIdx]);
+                    if(sr.ReadLine() == null){
+                        int index = rnd.Next(capitals.Count);
+                        randomCapital = capitals[index].ToUpper();
+                        countryHint = countries[index].ToUpper();
+                    }
+                }
+            }
+            
+            this.capital = randomCapital;
+            this.countryHint = countryHint;
         }
     }
 }
